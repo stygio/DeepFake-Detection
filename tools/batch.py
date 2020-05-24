Testing different normalization, fixed error with CUDA when running validation after each training epoch.

tools/batch.py
@@ -3,6 +3,7 @@ from PIL import Image
 import torch
 import time
 import os
+import numpy as np
 
 from models import transform
 from tools import preprocessing, opencv_helpers
@@ -303,6 +304,13 @@ class BatchGenerator:
 				misc.put_file_in_folder(file_path = video_path_2, folder = "bad_samples")
 
 
+	def show_tensor(self, tensor):
+		image = np.moveaxis(tensor.cpu().detach().numpy(), 0, -1) * 255
+		cv2.imshow("tensor", image)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+
 	"""
 	Function to retrieve a generator of training batches in tensor form (Kaggle dataset)
 	The batches contain sequences of consecutive frames from a two videos (half of the batch from each one)
@@ -349,7 +357,7 @@ class BatchGenerator:
 		if fake_data_type == 'images':
 			for frame_nr in frame_numbers:
 				fake_face = cv2.imread(os.path.join(fake_images_path, '{}.png'.format(frame_nr)))
-				fake_faces.append(self.tensor_transform(self.training_transform(Image.fromarray(fake_face))))			
+				fake_faces.append(self.tensor_transform(self.training_transform(transform.to_PIL(fake_face))) * (1./255))			
 		else:
 			try:
 				# Check that the video was opened successfully
@@ -365,7 +373,10 @@ class BatchGenerator:
 					left 	= fake_boxes[str(frame_numbers[i])]['0']['left']
 					right 	= fake_boxes[str(frame_numbers[i])]['0']['right']
 					fake_face = preprocessing.crop_image(fake_frames[i], (top, bottom, left, right))
-					fake_faces.append(self.tensor_transform(self.training_transform(Image.fromarray(fake_face))))
+					# preprocessing.show_test_img(fake_face)
+					fake_face = self.tensor_transform(self.training_transform(transform.to_PIL(fake_face))) * (1./255)
+					# self.show_tensor(fake_face)
+					fake_faces.append(fake_face)
 			except CorruptVideoError:
 				fake_video_handle.release()
 				# Move the file to a folder for corrupt videos
@@ -375,7 +386,7 @@ class BatchGenerator:
 		if real_data_type == 'images':
 			for frame_nr in frame_numbers:
 				real_face = cv2.imread(os.path.join(real_images_path, '{}.png'.format(frame_nr)))
-				real_faces.append(self.tensor_transform(self.training_transform(Image.fromarray(real_face))))
+				real_faces.append(self.tensor_transform(self.training_transform(transform.to_PIL(real_face))) * (1./255))
 		else:
 			try:
 				# Check that the video was opened successfully
@@ -392,7 +403,7 @@ class BatchGenerator:
 					left 	= real_boxes[str(frame_numbers[i])]['0']['left']
 					right 	= real_boxes[str(frame_numbers[i])]['0']['right']
 					real_face = preprocessing.crop_image(real_frames[i], (top, bottom, left, right))
-					real_faces.append(self.tensor_transform(self.training_transform(Image.fromarray(real_face))))
+					real_faces.append(self.tensor_transform(self.training_transform(transform.to_PIL(real_face))) * (1./255))
 			except CorruptVideoError:
 				real_video_handle.release()
 				# Move the file to a folder for corrupt videos
@@ -440,7 +451,7 @@ class BatchGenerator:
 				left 	= boxes[str(frame_numbers[i])]['0']['left']
 				right 	= boxes[str(frame_numbers[i])]['0']['right']
 				face = preprocessing.crop_image(frames[i], (top, bottom, left, right))
-				faces.append(self.tensor_transform(Image.fromarray(face)))
+				faces.append(self.tensor_transform(Image.fromarray(face)) * (1./255))
 
 			batch = torch.stack(faces).to(self.device)
 			return batch

tools/network.py
@@ -95,13 +95,6 @@ class Network:
 		
 		# Creating batch generator
 		BG = BatchGenerator(self.model_name, self.device, batch_size)
-		
-		# Unfreezing gradients
-		if finetuning_level == 'lower':
-			self.network.unfreeze_lower_level()
-		if finetuning_level == 'lower' or 'higher':
-			self.network.unfreeze_higher_level()
-		self.network.unfreeze_classifier()
 
 		# Get label tensor
 		labels = torch.tensor([0]*int(batch_size/2) + [1]*int(batch_size/2), device = self.device, requires_grad = False, dtype = torch.float)
@@ -143,11 +136,18 @@ class Network:
 		for epoch in range(1, epochs+1):
 			accuracies = []
 			errors = []
+
+			# Unfreezing gradients
+			if finetuning_level == 'lower':
+				self.network.unfreeze_lower_level()
+			if finetuning_level == 'lower' or 'higher':
+				self.network.unfreeze_higher_level()
+			self.network.unfreeze_classifier()
 			
 			# Initializing optimizer with appropriate lr
 			classifier_lr = lr * 0.8**(epoch-1)
-			higher_level_lr = 0.1 * classifier_lr
-			lower_level_lr = 0.01 * classifier_lr
+			higher_level_lr = 1. * classifier_lr
+			lower_level_lr = 1. * higher_level_lr
 			optimizer = optim.SGD([
 				{'params': self.network.classifier_parameters(), 'lr': classifier_lr},
 				{'params': self.network.higher_level_parameters(), 'lr': higher_level_lr},
@@ -226,6 +226,8 @@ class Network:
 								err, acc)
 					misc.add_to_log(log_file = iteration_log, log_string = log_string)
 
+			# Clean CUDA cache
+			torch.cuda.empty_cache()
 			# Save the model weights after each folder
 			self.save_model("ff_" + str(epoch) + "_", finetuning_level)
 
@@ -367,6 +369,10 @@ class Network:
 	def evaluate(self, dataset, dataset_path, mode, batch_size = 24):
 		
 		self.network.eval()
+		
+		# Disable gradients
+		for param in self.network.parameters():
+			param.requires_grad = False
 
 		# Creating batch generator
 		BG = BatchGenerator(self.model_name, self.device, batch_size)
@@ -465,5 +471,6 @@ class Network:
 				postfix_dict = {'loss': round(np.mean(overall_err), 2), 'acc': round(np.mean(overall_acc), 2)}
 				progress_bar.set_postfix(postfix_dict, refresh = False)
 
+		torch.cuda.empty_cache()
 		return {'loss': np.mean(overall_err), 'acc': np.mean(overall_acc)}
 